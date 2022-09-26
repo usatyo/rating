4254cc236f960913f66617cc0144839ebc97cb1e
usatyo <yu0424.math8128@gmail.com>
2022-09-26 14:20:32 +0900

    rate only integer

--- utils/util.ts
+++ utils/util.ts
@@ -10,7 +10,7 @@ export const calc_rate = async (
   const b_rate: number = await getRateFromName(b)
   const w_rate: number = await getRateFromName(w)
   const dif: number = (b_rate - w_rate + handi * 50) * result
-  const width: number = (dif / 800 + 0.5) * K
+  const width: number = Math.round((dif / 800 + 0.5) * K)
   const new_b: number = b_rate - width * result
   const new_w: number = w_rate + width * result
   return [new_b, new_w]
